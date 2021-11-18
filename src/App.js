b7753f1cdc84b605f56faa8a740b87f8414b0d6e
moriarty83 <moriarty83@gmail.com>
2021-11-17 22:02:24 -0500

    begin nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from './Styled Components/Button';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
+    <Header />
      <Button primary>Button</Button>
      <Button>Button</Button>
     </div>
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+
+function Header (props) {
+    return(
+    <nav>
+        <div className="menu-container">
+            <img className="menu-icon" src="./hamburger_menu.png" alt="" />
+        </div>
+    </nav>
+    )
+}
+
+export default Header;
