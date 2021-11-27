b025a24b6895b7923f3158ba92ef05c17d975d95
moriarty83 <moriarty83@gmail.com>
2021-11-26 19:36:36 -0500

    profile current past items

--- src/Components/UserItems.js
+++ src/Components/UserItems.js
@@ -11,24 +11,26 @@ const URL = "https://allston-christmas.herokuapp.com/api/profile/";
 const getUserItems = async () => {
     const response = await fetch(URL+props.email);
     const data = await response.json();
-    console.log(data);
     setUserItems(data);
 };
 
-
+const today = new Date();
+console.log("today: ", today)
 const loaded = () => {
-        
-    const myItems = userItems.map((item) => {         
+    const currentArr = userItems.filter((item)=> Date.parse(item.trashDay) > today)
+    const pastArr = userItems.filter((item)=> Date.parse(item.trashDay) < today)
+    console.log("Current Arr", currentArr);
+    const currentItems = currentArr.map((item) => {         
     return(
       
       <div key={item._id} className="item">
         <Link to={`/items/${item._id}`}><h1>{item.name}</h1></Link>
         {/* <h3>{item.description}</h3> */}
-        {/* <h3>{item.address}</h3> */}
-        {/* <h3>{(item.trashDay).slice(0,10)}</h3> */}
+        <h3>{item.address}</h3>
+        <h3>{(item.trashDay).slice(0,10)}</h3>
         <h3>{item.city}, {item.state}</h3>
 
-        {/* <h3>{item.zip}</h3> */}
+        <h3>{item.zip}</h3>
         {/* <h3>{item.latitude}</h3>
         <h3>{item.longitude}</h3> */}
         <img src={item.image_url} alt={item.name} />
@@ -36,8 +38,32 @@ const loaded = () => {
       </div>
     )}
     );
-    console.log(myItems);
-    return (<div className="items-div">{myItems}</div>)
+
+    const pastItems = pastArr.map((item) => {         
+        return(
+          
+          <div key={item._id} className="item">
+            <Link to={`/items/${item._id}`}><h1>{item.name}</h1></Link>
+            {/* <h3>{item.description}</h3> */}
+            <h3>{item.address}</h3>
+            <h3>{(item.trashDay).slice(0,10)}</h3>
+            <h3>{item.city}, {item.state}</h3>
+    
+            <h3>{item.zip}</h3>
+            {/* <h3>{item.latitude}</h3>
+            <h3>{item.longitude}</h3> */}
+            <img src={item.image_url} alt={item.name} />
+            
+          </div>
+        )}
+    );
+
+    return (<>
+    <h1 className="title">Current Items</h1>
+    <div className="items-div">{currentItems}</div>
+    <h1 className="title">Past Items</h1>
+    <div className="items-div">{pastItems}</div>
+    </>)
 };
 
 const loading = () => {
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,17 +6,22 @@ const Profile = () =>{
     const { user, isAuthenticated, isLoading } = useAuth0();
 
     if (isLoading){
-        return <div>Loading your info</div>
+        return <div className>Loading your info</div>
     }
 
     return(
         isAuthenticated && (
-            <div>
-                <img src={user.picture} alt={user.name} />
-                <h2>{user.name}</h2>
-                <p>{user.email}</p>
-                <UserItems email={user.email}/>
+            <>
+            <div className="items-div">
+                <div className="profile">
+                    <img src={user.picture} alt={user.name} />
+                    <h2>{user.name}</h2>
+                    {/* <p>{user.email}</p> */}
+                </div>
             </div>
+                <UserItems email={user.email}/>
+            </>
+            
         )
     );
 };
